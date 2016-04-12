87650c6b69fa7a8f1ca3382086db574544d54d8b
Wayne Scott <wayne-scott@users.noreply.github.com>
2016-04-12 20:36:35 +1000

    Added something else.

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
@@ -11,6 +7,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
+            Console.WriteLine("I need another change!");
         }
     }
 }
